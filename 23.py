2acc97c3cc98609157170ce53164d988d02dbc01
DevinMoreland <devinmoreland@gmail.com>
2022-12-12 18:01:03 -0500

    Needs Optimization

--- 23.py
+++ 23.py
@@ -7,3 +7,53 @@ As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest numb
 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 '''
+#%%
+import numpy as np
+import math
+
+iLim = 1
+uLim = 28123
+
+def getDivisors(n):
+    y = np.array([])
+    for i in range(1,math.ceil(n/2)+1):
+        if n%i== 0:
+            y=np.append(y,i)
+    return(y.astype(int))
+
+def isAbundent(x):
+    y = sum(getDivisors(x))
+    if y > x:
+        return(True)
+    else:
+        return(False)
+
+def abundentList(x,y):
+    z=np.array([])
+    if x < y: 
+        for i in range(x,y):
+            if isAbundent(i)==True:
+                z=np.append(z,i).astype(int)
+        return(z)
+    else:
+        print("fail")
+            
+    result = [False] * (lim + 1)
+    for i in range(len(y)):
+        for j in range(i, len(y)):
+            s = y[i] + y[j]
+            if s > lim:
+                break
+            result[s] = True
+            
+allAbundent = abundentList(iLim,uLim)
+
+for i in range(len(allAbundent)):
+    for j in range(i, len(allAbundent)):
+        s = allAbundent[i] + allAbundent[j]
+        if s > uLim:
+            break
+        result[s] = True
+
+print(sum(i for i, x in enumerate(result) if not x))
+# %%
